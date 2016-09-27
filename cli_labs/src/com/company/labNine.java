4861770300c841613e9482370b743ee1b10890e1
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-09-27 14:41:57 -0500

    "added that try catch for labNine"

--- cli_labs/src/com/company/labNine.java
+++ cli_labs/src/com/company/labNine.java
@@ -13,7 +13,15 @@ public class labNine {
 
         while(goOn.equalsIgnoreCase("y")) {
             System.out.println("Please enter the radius of your circle.");
-            double r = entry.nextDouble();
+            double r = 0;
+            try {
+                r = entry.nextDouble();
+            } catch (Exception e) {
+                // Read any pending input which would be invalid characters at this point
+                System.out.println("Whoops, we did not understand that input. Please enter that number again");
+                entry.next();
+                continue;
+            }
             //construct a new circle with the radius from user input
             Circle circle = new Circle(r);
             //call the Circle object created to modify the needed variables
